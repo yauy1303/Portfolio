a19b1b76b6aa86e004bb3e6445907c7ef8411072
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-11-11 11:00:59 +0000

    11/11サンプルseed完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 # admin = true
 sugi = Member.find_or_create_by!(email: "sugi@sugi") do |member|
   member.name = "sugi"
-  member.introduction = "よろしくお願いします！"
+  member.introduction = "愛犬との日常を楽しんでいます。"
   member.password = "123456789"
   member.password_confirmation = "123456789"
   member.admin = true
@@ -19,20 +19,54 @@ sugi = Member.find_or_create_by!(email: "sugi@sugi") do |member|
 end
 
 # admin = false
-alice = Member.find_or_create_by!(email: "alice@alice") do |member|
+alice = Member.find_or_create_by!(email: "email2@email") do |member|
   member.name = "alice"
-  member.introduction = "よろしくお願いします！"
-  member.password = "password1"
-  member.password_confirmation = "password1"
+  member.introduction = "ペットのラッキーと週末はドッグランに行ってます！"
+  member.password = "password2"
+  member.password_confirmation = "password2"
   member.admin = false
   member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/user_2.jpg"), filename:"user_2.jpg")
 end
 
+bob = Member.find_or_create_by!(email: "email3@email") do |member|
+  member.name = "bob"
+  member.introduction = "ワンちゃんとのコミュニケーションについて投稿します。"
+  member.password = "password3"
+  member.password_confirmation = "password3"
+  member.admin = false
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/user_3.jpg"), filename:"user_3.jpg")
+end
 
+carol = Member.find_or_create_by!(email: "email4@email") do |member|
+  member.name = "carol"
+  member.introduction = "犬と一緒にカワイイを楽しんでいます！"
+  member.password = "password4"
+  member.password_confirmation = "password4"
+  member.admin = false
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/user_4.jpg"), filename:"user_4.jpg")
+end
+
+dave = Member.find_or_create_by!(email: "email5@email") do |member|
+  member.name = "dave"
+  member.introduction = "僕もパートナーもグルメです！"
+  member.password = "password5"
+  member.password_confirmation = "password5"
+  member.admin = false
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/user_5.jpg"), filename:"user_5.jpg")
+end
+
+eve = Member.find_or_create_by!(email: "email6@email") do |member|
+  member.name = "eve"
+  member.introduction = "セラピードッグの活動を行っています。"
+  member.password = "password6"
+  member.password_confirmation = "password6"
+  member.admin = false
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/user_6.jpg"), filename:"user_6.jpg")
+end
 
 
 # 記事サンプル
-article1 = Article.find_or_create_by!(title: "のあです") do |article|
+article1 = Article.find_or_create_by!(title: "のあと散歩です") do |article|
   article.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. "
   article.member = sugi
   article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_1.jpg"), filename:"article_1.jpg")
@@ -44,22 +78,57 @@ article2 = Article.find_or_create_by!(title: "ドッグランで遊んだよ") d
   article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_2.jpg"), filename:"article_2.jpg")
 end
 
+article3 = Article.find_or_create_by!(title: "お手から始めるしつけの方法") do |article|
+  article.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. "
+  article.member = bob
+  article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_3.jpg"), filename:"article_3.jpg")
+end
+
+article4 = Article.find_or_create_by!(title: "愛犬にかわいい洋服を着せよう！") do |article|
+  article.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. "
+  article.member = carol
+  article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_4.jpg"), filename:"article_4.jpg")
+end
+
+article5 = Article.find_or_create_by!(title: "愛犬大喜び！絶品ごはんの作り方") do |article|
+  article.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. "
+  article.member = dave
+  article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_5.jpg"), filename:"article_5.jpg")
+end
+
+article6 = Article.find_or_create_by!(title: "セラピードッグイベントを開催します") do |article|
+  article.text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. "
+  article.member = eve
+  article.article_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/article_6.jpg"), filename:"article_6.jpg")
+end
+
+
 # タグサンプル
 tag1 = Tag.create(tag_name:"トイプードル")
 tag2 = Tag.create(tag_name:"散歩")
 tag3 = Tag.create(tag_name:"ゴールデンレトリバー")
 tag4 = Tag.create(tag_name:"ドッグラン")
+tag5 = Tag.create(tag_name:"しつけ")
+tag6 = Tag.create(tag_name:"コミュニケーション")
+tag7 = Tag.create(tag_name:"ファッション")
+tag8 = Tag.create(tag_name:"アイテム")
+tag9 = Tag.create(tag_name:"ごはん")
+tag10 = Tag.create(tag_name:"クッキング")
+tag11 = Tag.create(tag_name:"セラピードッグ")
+tag12 = Tag.create(tag_name:"イベント")
 
 article1.tags << tag1
 article1.tags << tag2
 article2.tags << tag3
 article2.tags << tag4
-
-
-
-
-
-
+article3.tags << tag5
+article3.tags << tag6
+article4.tags << tag7
+article4.tags << tag8
+article5.tags << tag9
+article5.tags << tag10
+article6.tags << tag11
+article6.tags << tag12
 
 
 # 5.times do |num|
