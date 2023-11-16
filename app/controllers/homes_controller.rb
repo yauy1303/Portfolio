9590d8ac69cffc30db61d5d0b197eba86fabb3c8
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-11-16 03:48:03 +0000

    11/16ランキング機能実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @tags = Tag.all
+    @article_like_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(3).pluck(:article_id))
   end
 
   def about
