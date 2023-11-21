d2668139b51d99b3ef7fa4faf70e1d615adb9734
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-11-21 13:35:55 +0000

    11/21タグ一覧・詳細完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   def top
     @article_like_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(3).pluck(:article_id))
     @new_article = Article.order(id: :DESC).limit(3)
-    @tag_list = Tag.order(:id).page(params[:page]).per(20)
+    @tag_list = Tag.order(:id).page(params[:page]).per(30)
   end
 
   def about
