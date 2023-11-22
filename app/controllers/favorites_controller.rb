a4a5e49536272e129f98dc7230f1922571cb1b22
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-11-22 11:32:57 +0000

    11/22いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,16 @@ class FavoritesController < ApplicationController
     @article = Article.find(params[:article_id])
     favorite = current_member.favorites.new(article_id: @article.id)
     favorite.save
-    redirect_to request.referer
+    # 非同期対応
+    # redirect_to request.referer
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     favorite = current_member.favorites.find_by(article_id: @article.id)
     favorite.destroy
-    redirect_to request.referer
+    # 非同期対応
+    # redirect_to request.referer
   end
 
 end
