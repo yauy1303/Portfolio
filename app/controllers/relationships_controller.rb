28c954491d1a2c0f09d08ebe45ab9ae3515f35db
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-11-22 13:04:04 +0000

    11/22フォロー非同期化投稿詳細まで

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,17 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_member!
 
   def create
-    member = Member.find(params[:member_id])
-    current_member.follow(member)
-    redirect_to request.referer
+    @member = Member.find(params[:member_id])
+    current_member.follow(@member)
+    # 非同期対応
+    # redirect_to request.referer
   end
 
   def destroy
-    member = Member.find(params[:member_id])
-    current_member.unfollow(member)
-    redirect_to request.referer
+    @member = Member.find(params[:member_id])
+    current_member.unfollow(@member)
+    # 非同期対応
+    # redirect_to request.referer
   end
 
   def followings
